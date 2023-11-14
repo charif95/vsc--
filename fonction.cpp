53b237b4aa42516544223fbe12a7bf0609d54d26
Charif95 <elkhamlichi95charif@gmail.com>
2023-11-14 22:34:32 +0100

    Fonction.cpp

---
+++ fonction.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void myFonction()
+{
+    cout << "*************************************\n\n";
+    cout << "Name: Charif el khamlichi.\n";
+    cout << "Age  : 28 years.\n";
+    cout << "City : Tetouan.\n";
+    cout << "Country: Morocco\n";
+    cout << "*************************************\n";
+}
+
+
+
+int main()
+{
+    myFonction();
+    myFonction();
+    myFonction();
+    myFonction();
+    myFonction();
+    myFonction();
+    myFonction();
+    
+}
