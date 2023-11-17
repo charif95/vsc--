fa53e4152325c7f45be3f502b144404ec0fe8fa7
Charif95 <elkhamlichi95charif@gmail.com>
2023-11-17 22:59:15 +0100

    Procedure.cpp

---
+++ Prob1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void MyProcedure1()
+{
+    string Charif;
+
+    cout <<" My Name is: Charif." << endl;
+
+}
+
+void MyProcedure2()
+{
+    string Name;
+    cout << "Enter your Name?" << endl;
+    cin >> Name;
+    cout << Name<< endl;
+
+}
+
+int main()
+{
+    MyProcedure1();
+    MyProcedure2();
+
+}
