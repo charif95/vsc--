389d32ce237d6d4498f0d0777dce5e31078d8fc5
Charif95 <elkhamlichi95charif@gmail.com>
2023-11-29 20:56:11 +0100

    forpr.cpp

---
+++ .vscode/ForProb46.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+// void printLetters()
+// {
+//     char start = 'A';
+//     char end = 'Z';
+
+//     for (char letter = start; letter <= end; ++letter) 
+//     {
+//     cout << letter << " ";
+//     }
+//     cout << std::endl;
+// }
+
+// int main() {
+//     printLetters();
+//     return 0;
+// }
+
+
+void printLetters() {
+
+    for (char l = 65; l <= 90; ++l) 
+    {
+    cout << l << " ";
+    }
+    cout << std::endl;
+}
+
+int main() {
+    printLetters();
+    return 0;
+}
+
+
+
