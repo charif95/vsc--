f595ab83883dbd27f64842974ec0cf2eab4236dc
Charif95 <elkhamlichi95charif@gmail.com>
2023-11-30 21:55:17 +0100

    NFL1.cpp

---
+++ .vscode/NFL1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    for (int i = 10 ; i >= 1; i--)
+    {
+
+        for (int j = i; j >= 1; j--)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+        
+    }
+    return 0;
+}
