e6fb61bfbb0c32b6ade6f95341afa7dffc36d589
Charif95 <elkhamlichi95charif@gmail.com>
2023-11-30 20:44:10 +0100

    NFL.coo

---
+++ .vscode/NestedForLoops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    for (char l = 65; l <= 90; l++)
+    {
+
+        for (char L = 65; L <= 90; L++)
+        {
+            cout << l << L << endl;
+        }
+        
+    }
+    return 0;
+}
