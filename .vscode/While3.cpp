5351e691f48c34c57ffe82e68d7aaac0ea53a193
Charif95 <elkhamlichi95charif@gmail.com>
2023-12-02 21:59:15 +0100

    While3.cpp

---
+++ .vscode/While3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1, Sum=0;
+        while (i <= 10)
+        {
+        if (i % 2 == 0)
+        {
+        Sum += i; 
+        }
+        i++;
+         
+    }
+
+    cout << "the sum of even numbers from 1 to 10 is: " << Sum << endl; 
+
+} 
+
+    
