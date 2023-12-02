909e089fe32c44fd42ac3cfb711528474be34717
Charif95 <elkhamlichi95charif@gmail.com>
2023-12-03 00:07:28 +0100

    While5.cpp

---
+++ .vscode/While5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int Num, M, Sum = 1, Count = 0;
+    cout << "Please enter the Num and M ?\n";
+    cin >> Num;
+    cin >> M;
+
+    while (Count < M)
+    {
+        Sum *=Num;
+        Count++;
+    }
+        cout << Num << " raised to the power of " << M << " is: " << Sum << endl;
+        
+    
+    
+}
