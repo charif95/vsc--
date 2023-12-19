3778ddba8b7c578e52751542f9c9fba79a3a3018
Charif95 <elkhamlichi95charif@gmail.com>
2023-12-19 23:49:08 +0100

    LWDW.cpp

---
+++ .vscode/LoopWithDoWhile052.cpp
@@ -0,0 +1,31 @@
+/*
+    Loop
+        Loop With Do...While
+
+    Syntax
+    do
+    {
+
+    } while (Condition Is True);
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int index = 6;
+/*     while (index < 6)
+    {
+        cout << index << "\n";
+        index++;
+    }
+ */   
+    do
+    {
+        cout << index << "\n";
+        index++;
+    } while (index < 6);
+    
+    return 0;
+}
