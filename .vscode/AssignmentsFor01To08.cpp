47b54ba3dba50ed1b57c25e0dedefa7ed11840d0
Charif95 <elkhamlichi95charif@gmail.com>
2023-12-24 22:42:45 +0100

    Ass02.cpp

---
+++ .vscode/AssignmentsFor01To08.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+int main()
+{
+    // Assignment 01
+    std::cout << "Line One\n";
+    std::cout << "Line Two\n";
+    std::cout << "Line Three\n";
+
+    std::cout << "\n==============================\n\n";
+
+    // Assignment 03
+    std::cout << "I am Learning Programming\n"
+    "With\n"
+    "Elzero\n"
+    "Web \n"
+    "School\n"
+    "<3\n";
+
+    std::cout << "\n==============================\n\n";
+
+    // Assignment 04
+    std::cout << "|=======================|\n";
+    std::cout << "|== Elzero Web School ==|\n";
+    std::cout << "|==        C++        ==|\n";
+    std::cout << "|=======================|\n";
+
+    // Assignment 05
+    std::cout << "\n==============================\n\n";
+
+    // This Is My First File
+    // In Learning Programming
+    // Iam Happy <3
+    // Iam Dragon
+
+    std::cout  << "Line One\n"; // Character Output
+    std::cout << "Line Two\n"; /* cout Function */
+    std::cout << "Line Three\n";
+
+  // Return 0
+  return 0;
+    // End Main Function 
+}  
---
+++ .vscode/AssignmentsFor09to015.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    /* // Assignment 09
+
+    int age = 40, period = 10;
+    
+    // Write Your Code Here
+    // Do Not Edit The Next 2 Lines
+    cout << "My Age Is: " << age << "\n";
+    cout << period << " Years Ago My Age Was: " << age - period << "\n";
+    cout << "After " << period << " Years My Age Will Be: " << age + period << "\n";
+    // Add The Third Line Below 
+    
+    cout << "======================================\n\n";
+
+    // Assignment 010
+    
+    // Write Your One Line Code Here
+    int
+     // Do Not Edit Below
+    a = 10, b = 20, c = 70;
+    cout << "Sum Of All Number Is: " << a + b + c << endl; 
+
+    cout << "======================================\n\n";
+
+    // Assignment 011
+
+    /*
+  name        [ Valid | Good Practice ]
+  NAME        [ Valid | Bad Practice ]
+  1name       [ Valid | Bad Practice ]
+  __name      [ Valid | Bad Practice ]
+  name@name   [ Valid | Bad Practice ]
+  name10name  [ Valid | Bad Practice ]
+  name!name   [ Valid | Bad Practice ]
+  first_NAME  [ Valid | Good Practice ]
+  first_name  [ Valid | Good Practice ]
+  firstName   [ Valid | Good Practice ]
+  first name  [ Valid | Bad Practice ]
+  fn          [ Valid | Bad Practice ]
+  public      [ Valid | Good Practice ]
+  Public      [ Valid | Good Practice ]
+*/
+   /* cout << "======================================\n";
+
+    // Assignment 012
+
+        // Write my_text Variable Content Here
+    string my_text = "== Elzero ==\n";
+
+        // Do Not Edit Below
+    cout << my_text;
+
+    cout << "======================================\n\n"; */
+
+        /* // Assignment 013
+
+        // Do Not Edit Below
+    int a, b, c = 100;
+
+        // Add Your Code Here
+
+        a = 50, b = 150;
+        // Do Not Edit Below
+    cout << a + b + c; // 300
+
+    cout << "======================================\n\n"; */
+
+        /* // Assignment 014
+
+    // Do Not Edit Below
+    int a = 210, b = -50, c = -40;
+
+    // Write Your cout Code Here
+
+    cout << a - b - c << endl; */
+
+    //cout << "======================================\n\n";
+
+    // Assignment 015
+
+    /* int num = 50;
+
+int read()
+{
+   int num = 100;
+  cout << num;
+  return 0;
+}
+
+int play()
+{
+  int num = 200;
+  read();
+  return 0;
+}
+
+int main()
+{
+  play();
+  return 0;
+} */
+
+
+
+   // cout << "======================================\n\n";
+
+    // Assignment 016
+
+
+    // Edit Here To Fix The Error
+  const int current_year = 2022;
+  const int birth_year = 1982;
+
+  // Do Not Edit Here
+  //current_year = 2022;
+  //birth_year = 1982;
+  int age_in_years = current_year - birth_year;
+  cout << age_in_years;
+  
+   return 0;
+
+}
