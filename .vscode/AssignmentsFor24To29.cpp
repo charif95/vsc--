bbea0b8a8901aac767185e4a48128e9ac9b10682
Charif95 <elkhamlichi95charif@gmail.com>
2024-01-06 22:07:27 +0100

    IncrDecr.cpp

---
+++ .vscode/AssignmentsFor24To29.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    /* // Assignment 01
+
+    int num = 3;
+    cout << num + num << "\n";                 // 6
+    cout << num * num - num << "\n";           // 6
+    cout << (num * num) - num << "\n";         // 6
+    cout << (num + num + num) - num << "\n";   // 6
+    cout << (num + num) + (num - num) << "\n"; // 6
+    cout << (num - num) + (num + num) << "\n"; // 6
+    cout << (num + num) - (num - num) << "\n"; // 6
+
+    cout <<"============================================\n\n";
+
+    // Assignment 02
+
+    cout << ((10 - 20) * (15 + 3) + (190 - 10)) * 400 << "\n"; // 0 */
+
+    /* cout <<"============================================\n\n";
+
+    // Assignment 03
+
+    // Do Not Edit
+    int num = 3;
+
+    num += num;
+    cout << num << "\n"; */
+
+    /* cout <<"============================================\n\n";
+
+    // Assignment 04
+
+    cout << int(10.5) * (5) + int(40.5) + int(10.1) << "\n"; // 100
+
+    cout <<"============================================\n\n";  */
+
+    // Assignment 05
+
+    /* char a = '\n';
+    char b = 'c';
+    char c = '\n';
+
+    cout << b * c + a << "\n";
+
+    cout <<"============================================\n\n"; */
+
+    // Assignment 06
+
+    /*
+        We Need To Use This Operators In Solutions
+    - &&
+    - >=
+    - <=
+    - >
+    - <
+    - ==
+    - !
+    - ||
+*/
+
+/* int a = 10;
+int b = 25;
+int c = 15;
+
+cout << (a <= b) << "\n";          // True
+cout << (c >= a) << "\n";          // True
+cout << (a + c || b) << "\n";     // True
+cout << (c > a) << "\n";         // True
+cout << (c < b) << "\n";         // True
+cout << (b > a && b >= c) << "\n"; // True
+cout << !(a == b) << "\n";         // True
+cout << (a < b && b > a) << "\n"; // True */
+
+/* cout <<"============================================\n\n";
+
+// Assignment 07
+
+int a = 10;
+int b = 20;
+
+cout << ((a + b) * (a + b) + (a * a)) * a << "\n";       // 10000
+
+// Hint For Help
+cout << 10 + 5 * 20 << "\n";   // 5 * 20 = 100 + 10 = 110
+cout << (10 + 5) * 20 << "\n"; // 10 + 5 = 15 * 20 = 300 */
+
+cout <<"============================================\n\n";
+
+// Assignment 08
+
+int num = 10;
+cout << --num << " | " << num++ << " | " << ++num << "\n"; // 9 | 9 | 11
+cout << num++ << " | " << num++ << " | " << --num << "\n"; // 11 | 12 | 12
+cout << --num << " | " << --num << " | " << --num << "\n"; // 11 | 10 | 9
+    return 0;
+}
