b250feb897f113d4d06bab371c918c17d71da33f
Charif95 <elkhamlichi95charif@gmail.com>
2024-01-09 21:40:58 +0100

    AssSwitch.cpp

---
+++ .vscode/AssignmentsFor036To037.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Assignment 01
+
+    /* int year;
+
+    cout << "Please Enter The Year\n";
+    cin >> year;
+
+    switch (year)
+    {
+    case 1982:
+        cout << "My Birth Day\n";
+        break;
+    case 1989:
+        cout << "My First Work\n";
+        break;
+    case 1995:
+        cout << "Windows 95\n";
+        break;
+    case 2000:
+        cout << "Windows Millennium\n";
+        break;
+    case 2002:
+        cout << "Created My vBulletin Forum\n";
+        break;
+    
+    default:
+        cout << "No Events In This Year\n";
+        break;
+    } */
+
+    /* cout <<"\n============================================\n\n";
+
+    // Assignment 02
+
+    int day;
+    cin >> day;
+
+    switch (day)
+    {
+    case 1:
+    case 2:
+    case 3:
+        cout << "Shop Is Open\n";
+        break;
+    case 4:
+    case 5:
+        cout << "Shop Is Closed\n";
+        break;
+    default:
+        cout << "Day Is Not Valid\n";
+        break;
+    } */
+
+    cout <<"\n============================================\n\n";
+
+    // Assignment 03
+
+    int num;
+    cin >> num;
+
+    switch (num)
+    {
+    case 10:
+        cout << "case 1\n";
+        break;
+    case 20:
+        cout << "case 2\n";
+        break;
+    case 30:
+    case 31:
+    case 32:
+        cout << "case 3\n";
+        break;
+    
+    default:
+        cout << "Invalid Number\n";
+        break;
+    }
+
+    return 0;
+}
